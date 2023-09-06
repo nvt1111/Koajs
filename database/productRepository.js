0496cddb0f20a0ddbbbf9307bd75a65ad366fc50
anhth1 <114393245+anhth1@users.noreply.github.com>
2023-09-06 14:39:10 +0700

    aad more todo

--- database/productRepository.js
+++ database/productRepository.js
@@ -5,6 +5,7 @@ const testdata = fs.readFileSync('./database/products.json', 'utf-8');
 const productData = JSON.parse(testdata);//Object
 const products = productData.data;
 
+// sao lại phải { sort = "asc", limit } = {} như này nhỉ ? 
 export function getProducts({ sort = "asc", limit } = {}) {
 
     let product = [...products];
@@ -20,7 +21,7 @@ export function getProducts({ sort = "asc", limit } = {}) {
 }
 
 export function getOne(id) {
-
+    //todo: viết 1 hàm pick những fields cần thiết cho anh nhé . 
     return products.find(pro => pro.id == (id));
 }
 
@@ -35,19 +36,24 @@ export function updateById(id, data) {
 
     const productIndex = products.findIndex(pro => pro.id == (id));
 
+    // todo: chỗ này em thay vì em phải đẩy full data thì em chỉ đẩy những fiedls cần update còn các fields khác giữ nguyên nhé .
     const productNew = { id: id, createdAt: new Date().toISOString(), ...data };
 
     products[productIndex] = productNew;
+    //todo: đoạn này dùng đi dùng lại nhiều lần , viết ra 1 hàm rieeng nhé . 
 
     fs.writeFileSync('./database/products.json', JSON.stringify({ data: products }))
 }
 
 export function deleteById(id) {
 
+    //todo: dùng !== nhé . hạn chế hoặc không dùng !=
     const productList = products.filter(product => product.id != (id));
 
     fs.writeFileSync('./database/products.json', JSON.stringify({ data: productList }))
 }
+
+//todo: cái gì không dùng nữa thì xóa đi nhé 
 
 // export const sortProduct = (value, data) => {
 //     data.sort((a, b) => (value === 'desc') ? new Date(b.createdAt) - new Date(a.createdAt) : new Date(a.createdAt) - new Date(b.createdAt));
